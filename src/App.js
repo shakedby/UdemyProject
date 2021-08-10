67730e5606468324a2660c957eb01dcef718071a
shaked <by181996@gmail.com>
2021-08-10 13:35:06 +0300

    10/8

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+
+    <div className="Appp">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
